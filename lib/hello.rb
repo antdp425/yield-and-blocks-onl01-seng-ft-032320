863c1110cf26204c7c6af116c1885d0cacd29eb0
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-25 09:35:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,18 @@
 def hello_t (array)
-  i = 0
-  #First time through loop = 0, then 1, then so on
-  while i < array.length
-    #During each step of while loop, i will equal an index of our array
-    #We can use this information to yield each successive value in array
-    #Yield is saying "Go get the block of code down there, and dont come back unti you're done"
-    yield array[i]
-    i = i + 1
+  if block_given?
+    i = 0
+    #First time through loop = 0, then 1, then so on
+    while i < array.length
+      #During each step of while loop, i will equal an index of our array
+      #We can use this information to yield each successive value in array
+      #Yield is saying "Go get the block of code down there, and dont come back unti you're done"
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
   end
-  array
 end
 
 # call your method here!
